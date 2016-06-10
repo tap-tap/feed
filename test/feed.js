77be8771cd631f6726f0d00049be29ac9a9818da
AndreasZeiser <tome@andreaszeiser.de>
2016-06-10 17:15:59 +0200

    add webfeeds spec

--- test/feed.js
+++ test/feed.js
@@ -13,6 +13,14 @@ describe('RSS 2.0', function () {
       copyright: 'All rights reserved 2013, John Doe',
       updated: new Date(2013, 06, 14), // optional, default = today
 
+      webfeeds: {
+        accentColor: '#969b34',
+        analytics: '123abc',
+        cover: 'http://example.com/images/cover.jpg',
+        icon: 'http://example.com/images/icon.jpg',
+        logo: 'http://example.com/images/logo.svg'
+      },
+
       author: {
         name: 'John Doe',
         email: 'johndoe@example.com',
@@ -23,18 +31,22 @@ describe('RSS 2.0', function () {
     var output = '';
 
     output += '<?xml version="1.0" encoding="utf-8"?>\n';
-    output += '<rss version="2.0">\n';
+    output += '<rss version="2.0" xmlns:webfeeds="http://webfeeds.org/rss/1.0">\n';
     output += '    <channel>\n';
     output += '        <title>Feed Title</title>\n';
     output += '        <description>This is my personnal feed!</description>\n';
     output += '        <link>http://example.com/</link>\n';
-    output += '        <lastBuildDate>Sat, 13 Jul 2013 23:00:00 GMT</lastBuildDate>\n';
-    output += '        <docs>http://blogs.law.harvard.edu/tech/rss</docs>\n';
+    output += '        <lastBuildDate>'+ feed.updated.toUTCString() +'</lastBuildDate>\n';
     output += '        <image>\n';
     output += '            <title>Feed Title</title>\n';
     output += '            <url>http://example.com/image.png</url>\n';
     output += '            <link>http://example.com/</link>\n';
     output += '        </image>\n';
+    output += '        <webfeeds:cover image="http://example.com/images/cover.jpg"/>\n';
+    output += '        <webfeeds:icon>http://example.com/images/icon.jpg</webfeeds:icon>\n';
+    output += '        <webfeeds:logo>http://example.com/images/logo.svg</webfeeds:logo>\n';
+    output += '        <webfeeds:accentColor>#969b34</webfeeds:accentColor>\n';
+    output += '        <webfeeds:analytics id="123abc" engine="GoogleAnalytics"/>\n';
     output += '        <copyright>All rights reserved 2013, John Doe</copyright>\n';
     output += '        <generator>Feed for Node.js</generator>\n';
     output += '    </channel>\n';
